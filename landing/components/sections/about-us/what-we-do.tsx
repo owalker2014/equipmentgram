77fef82b2d1d374dd6810d2ab34588258fe834c1
emma-ng <emadimabua@gmail.com>
2026-01-03 20:13:15 +0100

    ui updates

--- landing/components/sections/about-us/what-we-do.tsx
+++ landing/components/sections/about-us/what-we-do.tsx
@@ -14,12 +14,12 @@ const WhatWeDo: React.FC<Props> = ({ isLanding = false }) => {
       <div className="container max-w-7xl mx-auto">
         <Title className="text-center pt-0">About Us</Title>
 
-        <Text size="sm" className="text-center mb-5">
+        <Text size="sm" className="text-center text-gray-600 mb-7">
           Learn about what EquipmentGram does
         </Text>
 
         <div className="grid md:grid-cols-2 grid-cols-1 md:gap-10 gap-10">
-          <div className="space-y-2 px-5">
+          <div className="space-y-2 px-5 py-5 bg-white">
             <Title className="text-blue-700">What We Do</Title>
 
             <Text c="dimmed" className="">
@@ -35,7 +35,7 @@ const WhatWeDo: React.FC<Props> = ({ isLanding = false }) => {
               easily shared.
             </Text>
           </div>
-          <div className="space-y-2 px-5">
+          <div className="space-y-2 px-5 py-5 bg-white">
             <Title className="text-blue-700">Our Mission</Title>
 
             <Text c="dimmed" className="">
