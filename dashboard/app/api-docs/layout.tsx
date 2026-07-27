2d907f9001f24c5f62111273988ab3f8da293e6d
emma-ng <emadimabua@gmail.com>
2026-07-27 04:56:11 +0100

    chore: fix vercel build

--- dashboard/app/api-docs/layout.tsx
+++ dashboard/app/api-docs/layout.tsx
@@ -1,14 +1,9 @@
 import "swagger-ui-react/swagger-ui.css";
-// import { notFound } from "next/navigation";
 
 export default function ApiDocsLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  // if (process.env.NODE_ENV === "production") {
-  //   notFound();
-  // }
-
   return <>{children}</>;
 }
